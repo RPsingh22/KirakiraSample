59894407add18ee438b3d1276731e8d48558c383
Gaurav Jindal <gauravjindal@Gaurav-Jindal.local>
2017-11-16 11:42:11 +0530

    Added project files

---
+++ KirakiraSample/KirakiraSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KirakiraSample
+//
+//  Created by Gaurav Jindal on 11/16/17.
+//  Copyright © 2017 Gaurav Jindal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
