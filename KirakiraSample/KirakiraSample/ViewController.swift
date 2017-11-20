10cb51271dbd85d3253f4ab582d6ab2c23c4cd08
Itesh Dutt <iteshdutt@IteshDutt-IPhone.local>
2017-11-20 14:55:38 +0530

    Camera integration for glitter effect

--- KirakiraSample/KirakiraSample/ViewController.swift
+++ KirakiraSample/KirakiraSample/ViewController.swift
@@ -7,19 +7,194 @@
 //
 
 import UIKit
+//import Twinkle
+import MobileCoreServices
+import ARKit
+import SceneKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,ARSCNViewDelegate {
+    let cameraController:UIImagePickerController = UIImagePickerController()
+    var isFirstTime:Bool = true
+    var nodes: [SphereNode] = []
 
+    lazy var sceneView: ARSCNView = {
+        let view = ARSCNView(frame: CGRect.zero)
+        return view
+    }()
+    
+    //2
+    lazy var infoLabel: UILabel = {
+        let label = UILabel(frame: CGRect.zero)
+        label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.title1)
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sceneView.delegate = self
+         view.addSubview(sceneView)
+        view.addSubview(infoLabel)
+        self.view.backgroundColor = UIColor.blue
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapRecognizer.numberOfTapsRequired = 1
+        sceneView.addGestureRecognizer(tapRecognizer)
+        /*
+        // using the UIView extension
+        let view: UIView = UIView(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+//        view.backgroundColor = UIColor.orange
+        self.view.addSubview(view)
+        view.twinkle()
+
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(twinkleView), userInfo: nil, repeats: true)
+        */
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //4
+        sceneView.frame = view.bounds
+         infoLabel.frame = CGRect(x: 0, y: 16, width: view.bounds.width, height: 64)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //5
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if isFirstTime == true {
+            isFirstTime = false
+//            _ = startCameraFromViewController(viewController: self, withDelegate: self )
+        }
 
+    }
+    
+    func startCameraFromViewController(viewController: UIViewController, withDelegate delegate: UIImagePickerControllerDelegate & UINavigationControllerDelegate) -> Bool {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == false {
+            return false
+        }
+        
+        cameraController.sourceType = .camera
+        cameraController.mediaTypes = [kUTTypeMovie as NSString as String]
+        cameraController.allowsEditing = false
+        cameraController.delegate = delegate
+        
+        self.present(cameraController, animated: true, completion: nil)
+        return true
+    }
+    
+    func video(videoPath: NSString, didFinishSavingWithError error: NSError?, contextInfo info: AnyObject) {
+        var title = "Success"
+        var message = "Video was saved"
+        if let _ = error {
+            title = "Error"
+            message = "Video failed to save"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: Gesture handlers
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        return;
+        //2
+        let tapLocation = sender.location(in: sceneView)
+        //3
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .featurePoint)
+        if let result = hitTestResults.first {
+            //4
+            let position = SCNVector3.positionFrom(matrix: result.worldTransform)
+            //5
+            let sphere = SphereNode(position: position)
+            //6
+            sceneView.scene.rootNode.addChildNode(sphere)
+            let lastNode = nodes.last
+            nodes.append(sphere)
+            if lastNode != nil {
+                //7
+                let distance = lastNode!.position.distance(to: sphere.position)
+                infoLabel.text = String(format: "Distance: %.2f meters", distance)
+            }
+        }
+ 
+    }
+        
+    func twinkleView()  {
+//        Twinkle.twinkle(self.view)
+
+    }
+    // MARK: ARSCNViewDelegate
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        //5
+        var status = "Loading..."
+        switch camera.trackingState {
+        case ARCamera.TrackingState.notAvailable:
+            status = "Not available"
+        case ARCamera.TrackingState.limited(_):
+            status = "Analyzing..."
+        case ARCamera.TrackingState.normal:
+            status = "Ready"
+        }
+        infoLabel.text = status
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        dismiss(animated: true, completion: nil)
+        
+        // Handle a movie capture
+        if mediaType == kUTTypeMovie {
+            guard let path = (info[UIImagePickerControllerMediaURL] as! NSURL).path else { return }
+            if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path) {
+                //                UISaveVideoAtPathToSavedPhotosAlbum(path, self, #selector(RecordVideoViewController.video(_:didFinishSavingWithError:contextInfo:)), nil)
+            }
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+        
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        print("updateAtTime")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        print("updateAtTime")
 
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("didRenderScene scene: SCNScene, atTime")
+        print(scene.lightingEnvironment)
+//        print(scene.reflec)
+
+    }
+}
+
+
+
+extension SCNVector3 {
+    func distance(to destination: SCNVector3) -> CGFloat {
+        let dx = destination.x - x
+        let dy = destination.y - y
+        let dz = destination.z - z
+        return CGFloat(sqrt(dx*dx + dy*dy + dz*dz))
+    }
+    
+    static func positionFrom(matrix: matrix_float4x4) -> SCNVector3 {
+        let column = matrix.columns.3
+        return SCNVector3(column.x, column.y, column.z)
+    }
 }
 
